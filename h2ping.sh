18e3168a7ca17fd7c9117a37b48fbdfa47badcc1
HÜSEYİN ÖZBUNAR <huseyinozbunar@stu.aydin.edu.tr>
2025-02-24 11:00:43 +0300

    pinging

---
+++ h2ping.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo"pinglemek istediğiniz adresi girin:"[3~
+read $site
+[A[C[C[C[C[C[C[C[C[adres
+[A[C[C[C[C[C[C[C[C[Cwebsite
+ping www.arabam.com
+[A[C[C[C[C[C[C[C[C[C[C[C[C[ping $website
